1192f25be88b88e67abe24c11997f57c087727fa
Jayden Bryant <jayden.bryant@me.com>
2019-04-29 11:33:25 -0700

    fix spelling errors

--- foods.js
+++ foods.js
@@ -1,4 +1,4 @@
 const food = ['pizza','apple','ice cream']
 const pizza: {
-	toppings: ['tomato', 'blue cheese', 'cheasy cheese']
+	toppings: ['tomato', 'blue cheese', 'cheeeesy cheese', 'nutmeg orange']
 }
