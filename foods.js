bb5182e397a2c6d71bae1ea95b6fcf57d053a631
Jayden Bryant <jayden.bryant@me.com>
2019-04-29 11:35:21 -0700

    couple more toppings

--- foods.js
+++ foods.js
@@ -1,4 +1,4 @@
 const food = ['pizza','apple pieces','ice cream', 'pineapple' 'resolved commit']
 const pizza: {
-	toppings: ['tomato', 'blue cheese', 'cheasy cheese']
+	toppings: ['tomato', 'blue cheese', 'cheasy cheese', 'harem cream']
 }
